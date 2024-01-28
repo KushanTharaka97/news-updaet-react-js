1fdb3db88eb8b00936c5449420545dfdad5f5d47
KushanTharakaPeiris <kushan.tharaka@axiatadigitllabs.com>
2024-01-28 21:01:07 +0530

    initial coomit

---
+++ src/News.js
@@ -0,0 +1,47 @@
+// src/News.js
+import React, { useState, useEffect } from 'react';
+
+const News = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const apiKey = '67cd4d6317cf4560a091cb95bc102b77'; // Replace with your News API key
+    const category = 'entertainment'; // Set the category to 'entertainment' for celebrity news
+  const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching news:', error);
+        setLoading(false);
+      });
+  }, []);
+
+ 
+  return (
+    <div>
+      <h2>Latest News</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {news.map((article) => (
+            <li key={article.title}>
+              <img src={article.urlToImage} alt={article.title} style={{ maxWidth: '100%', maxHeight: '150px' }} />
+              <strong>{article.title}</strong>
+              <p>{article.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default News;
